0d684f6e796eec8a44a50142b2c515469d96383e
Daniel <benjad@med.umich.edu>
2025-03-13 09:05:15 -0400

    Need to find a better test datset to proceed. This one is missing controls for some reason?

--- test.R
+++ test.R
@@ -4,7 +4,8 @@ source("DE_tools.R")
 x<- "test_data/counts.tsv"
 m<- "test_data/meta.tsv"
 
-#bad example need to delete some columns
+# bad example need to delete some columns with this jank data should really consider
+# a better test.
 
 counts<-read.delim(x)
 meta<-read.delim(m)
@@ -12,9 +13,11 @@ meta<-read.delim(m)
 meta <- meta[-c(8,11:13),] #removing extra columns don't replicate
 
 factor1<-colnames(meta)[6]
-factor2<-colnames(meta)[8]
+factor2<-colnames(meta)[12]
 
-f<-""
+f<-sprintf("~%s+%s",factor1,factor2)
+
+# Main functionality test -----
 
 # out<-DE_prep(counts,meta,f)
 # working as intended
@@ -39,3 +42,24 @@ out<-DE_run(counts,meta)
 # x,m <- df: works
 # x,m <- .csv: unknown
 # x,m <- .tsv: works
+
+# Visualization: never done unit testing before but would be better to have small
+# test cases baked into like a vignette of the package
+#
+# something like
+# test_DE_prep<-function(exampleargs)){
+#   if(expected_results == DE_prep(exampleargs)){print("Working as intended")}
+#   }
+#
+# Should probably read other packages to see how they handle testing but it's kind
+# of fun to wing it and see how others do it after
+
+## Visualization Tests ----------
+
+# assuming you have output
+# out<-DE_run(x,m,f)
+
+results(out,contrast("something useful with new dataset"))
+# yeah it's time to find a new dataset that actually has some differences
+
+
